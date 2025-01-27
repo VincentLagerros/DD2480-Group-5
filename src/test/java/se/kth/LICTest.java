7be2b02d192807718627e8f8d13208443c9f822d
ErikBetzholtz <erik.betzholtz@gmail.com>
2025-01-27 18:38:39 +0100

    feat: add method for LIC3
    This commit will add a method for LIC3 in the Main class for later integration into the DECIDE method. It will also create a new file containing tests for LIC3 and is intended for future tests of other LIC methods

---
+++ src/test/java/se/kth/LICTest.java
@@ -0,0 +1,50 @@
+package se.kth;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class LICTest {
+    @Test
+    public void testLIC3Positive(){
+        Main m = new Main();
+        double[] x = new double[10];
+        double[] y = new double[10];
+        x[9]=5;
+        y[9]=5;
+        x[8]=(-5);
+        y[8]=5;
+        double area = 0.5;
+
+        assertTrue(m.LIC3(x,y,area));
+    }
+
+    @Test
+    public void testLIC3NegativeForNoTriangle(){
+        Main m = new Main();
+        double[] x = new double[10];
+        double[] y = new double[10];
+        x[9]=5;
+        y[9]=5;
+        double area = 0.5;
+
+        assertFalse(m.LIC3(x,y,area));
+    }
+
+    @Test
+    public void testLIC3NegativeForLargeAreaThreshold(){
+        Main m = new Main();
+        double[] x = new double[10];
+        double[] y = new double[10];
+        x[9]=5;
+        y[9]=5;
+        x[8]=(-5);
+        y[8]=5;
+        double area = 50;
+
+        assertFalse(m.LIC3(x,y,area));
+    }
+}
