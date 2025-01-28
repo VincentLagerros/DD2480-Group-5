e1d270c9eb1db87d53a6da4f63ef5668f713f80c
ErikBetzholtz <erik.betzholtz@gmail.com>
2025-01-28 14:00:46 +0100

    refactor: move content from LICTest to EvaluateLICTest and move LIC3 to EvaluateLIC
    This commit will move tests for LIC from the deleted LICTest to EvaluateLICTest and LIC3 from Main to EvaluateLIC. This is to clean up after local merge with main

--- src/main/java/se/kth/Main.java
+++ src/main/java/se/kth/Main.java
@@ -5,31 +5,5 @@ public class Main {
         System.out.print("Hello world");
     }
 
-    /**
-     * Calculates if there exists three consecutive datapoints where the triangle formed from 
-     * using them as vertecies has an area that exceeds the specified area threshold. 
-     *
-     * @param xCoordinates  an array of the x-coordinates for the datapoints 
-     * @param yCoordinates  an array of the y-coordinates for the datapoints
-     * @param areaThreshold the area which the triangle must exceed
-     * @return              true if the three datapoints exist, otherwise false
-     */
-    public boolean LIC3(double[] xCoordinates, double[] yCoordinates, double areaThreshold){
-        if(areaThreshold<0) return false;
-
-        double x1, x2, x3, y1, y2, y3;
-
-        for(int i=0;i<xCoordinates.length-2;i++){
-            //get coordinates for the three datapoints
-            x1 = xCoordinates[i];
-            y1 = yCoordinates[i];
-            x2 = xCoordinates[i + 1];
-            y2 = yCoordinates[i + 1];
-            x3 = xCoordinates[i + 2];
-            y3 = yCoordinates[i + 2];
-
-            if(0.5*(Math.abs(x1*y2+x2*y3+x3*y1-x1*y3-x2*y1-x3*y2))>areaThreshold) return true;
-        }
-        return false;
-    }
+   
 }
--- src/test/java/se/kth/EvaluateLICTest.java
+++ src/test/java/se/kth/EvaluateLICTest.java
@@ -7,6 +7,49 @@ import org.junit.Test;
 // Class for all unit tests relating to LIC evaluation
 public class EvaluateLICTest {
     @Test
+    public void testLIC3Positive(){
+        //Case where a valid input is given
+        EvaluateLIC m = new EvaluateLIC();
+        double[] x = new double[10];
+        double[] y = new double[10];
+        x[9]=5;
+        y[9]=5;
+        x[8]=(-5);
+        y[8]=5;
+        double area = 0.5;
+
+        assertTrue(m.LIC3(x,y,area));
+    }
+
+    @Test
+    public void testLIC3NegativeForNoTriangle(){
+        //Case where no points create a triangle with area>0
+        EvaluateLIC m = new EvaluateLIC();
+        double[] x = new double[10];
+        double[] y = new double[10];
+        x[9]=5;
+        y[9]=5;
+        double area = 0.5;
+
+        assertFalse(m.LIC3(x,y,area));
+    }
+
+    @Test
+    public void testLIC3NegativeForLargeAreaThreshold(){
+        //Case where area threshold is larger than the triangle created by the points
+        EvaluateLIC m = new EvaluateLIC();
+        double[] x = new double[10];
+        double[] y = new double[10];
+        x[9]=5;
+        y[9]=5;
+        x[8]=(-5);
+        y[8]=5;
+        double area = 50;
+
+        assertFalse(m.LIC3(x,y,area));
+    }
+
+    @Test
     public void testLIC6Positive(){
         // Case where at least one point lies farther than dist from the line
         EvaluateLIC eval = new EvaluateLIC();
