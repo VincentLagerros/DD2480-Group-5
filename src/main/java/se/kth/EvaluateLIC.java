e053a334d508b6b046b52a8c5940c0b85cbfc12a
RasmussBrodin <rasmuss.brodin@gmail.com>
2025-01-28 14:27:49 +0100

    Merge branch main
    Merged main into feature/issue#11

--- src/main/java/se/kth/EvaluateLIC.java
+++ src/main/java/se/kth/EvaluateLIC.java
@@ -1,23 +1,6 @@
 package se.kth;
 
 public class EvaluateLIC {
-    
-    /**
-     * Calculates if there exists at least one set of two consecutive data points, (X[i],Y[i]) and (X[j],Y[j]), 
-     * suchthat X[j] - X[i] < 0. (where i = j-1)
-     *
-     * @param xCoordinates  An array of the x-coordinates for the datapoints 
-     * @param numpoints     Number of points in the dataset 
-     * @return              True if such a set exists, False otherwise
-     */
-    public boolean LIC5(double[] xCoordinates, int numPoints){
-        for(int j = 1; j < numPoints; j++){
-            if(xCoordinates[j] - xCoordinates[j-1] < 0){
-                return true;
-            }
-        }
-       return false;
-    }
 
      /**
      * Calculates if there exists three consecutive datapoints where the triangle formed from 
@@ -47,6 +30,23 @@ public class EvaluateLIC {
         return false;
     }
     
+    /**
+     * Calculates if there exists at least one set of two consecutive data points, (X[i],Y[i]) and (X[j],Y[j]), 
+     * suchthat X[j] - X[i] < 0. (where i = j-1)
+     *
+     * @param xCoordinates  An array of the x-coordinates for the datapoints 
+     * @param numpoints     Number of points in the dataset 
+     * @return              True if such a set exists, False otherwise
+     */
+    public boolean LIC5(double[] xCoordinates, int numPoints){
+        for(int j = 1; j < numPoints; j++){
+            if(xCoordinates[j] - xCoordinates[j-1] < 0){
+                return true;
+            }
+        }
+       return false;
+    }
+
     /**
      * Calculates if there exists at least one set of nPts consecutive data points such that at least one of the
      * points lies a distance greater than dist from the line joining the first and last of these nPts points.
