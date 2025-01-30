6be16979000a21cba987f6d74d82abc3cf10931b
Vincent <vincent.lagerros@gmail.com>
2025-01-30 16:43:52 +0100

    Refactor: Move to static, Closes #55 and #56
    Formats all the code, made all functions that should be static, into
    static. Fixed a bug in LIC12, and updated the test cases

--- src/test/java/se/kth/GlobalDeclarationsTest.java
+++ src/test/java/se/kth/GlobalDeclarationsTest.java
@@ -4,10 +4,10 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class GlobalDeclarationsTest {
-
     @Test
     public void testGlobalInitialization() {
         GlobalDeclarations.Globals globals = new GlobalDeclarations.Globals();
