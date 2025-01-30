5ea1141c55e6849e3359e464830597cbdfd1272d
RasmussBrodin <rasmuss.brodin@gmail.com>
2025-01-30 12:05:41 +0100

    Issue#20 added 2 negative test cases

--- src/test/java/se/kth/MainTest.java
+++ src/test/java/se/kth/MainTest.java
@@ -1,5 +1,6 @@
 package se.kth;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
@@ -14,10 +15,10 @@ public class MainTest {
         globals.yCoordinates = new double[]{0, 1, 0, 1, 0, 0, 2, 1, -2, 0, 0, 0, 12, 12};
 
         // Set LIC parameters to ensure conditions are met
-        globals.parameters.LENGTH1 = 0.5;   // Will trigger LIC0
-        globals.parameters.RADIUS1 = 1.0;   // Will trigger LIC1
-        globals.parameters.EPSILON = 0.1;   // Will trigger LIC2
-        globals.parameters.AREA1 = 0.5;     // Will trigger LIC3
+        globals.parameters.LENGTH1 = 0.5;   
+        globals.parameters.RADIUS1 = 1.0;   
+        globals.parameters.EPSILON = 0.1;   
+        globals.parameters.AREA1 = 0.5;    
         globals.parameters.Q_PTS = 3;
         globals.parameters.QUADS = 1;
         globals.parameters.N_PTS = 3;
@@ -34,10 +35,9 @@ public class MainTest {
         globals.parameters.RADIUS2 = 2.0;
         globals.parameters.AREA2 = 1.0;
     
-        // Set LCM to ensure PUM becomes true for all indices
         for (int i = 0; i < 15; i++) {
             for (int j = 0; j < 15; j++) {
-                globals.lcm[i][j] = GlobalDeclarations.Connectors.ORR;  // Ensures PUM[i][j] = true
+                globals.lcm[i][j] = GlobalDeclarations.Connectors.ORR;
             }
         }
     
@@ -45,5 +45,89 @@ public class MainTest {
         Main.DECIDE(globals);
 
         assertTrue(globals.launchDecision);
+    }
+
+    @Test
+    public void testInValidIputForDecide(){
+        GlobalDeclarations.Globals globals = new GlobalDeclarations.Globals();
+    
+        // Set number of points
+        globals.numPoints = 5;
+        globals.xCoordinates = new double[]{0, 1, 2, 3, 4};
+        globals.yCoordinates = new double[]{0, 1, 0, 1, 0};
+
+        // Set LIC parameters to ensure conditions are not met
+        globals.parameters.LENGTH1 = 0.5;   
+        globals.parameters.RADIUS1 = 1.0;   
+        globals.parameters.EPSILON = 0.1;   
+        globals.parameters.AREA1 = 0.5;     
+        globals.parameters.Q_PTS = 3;
+        globals.parameters.QUADS = 1;
+        globals.parameters.N_PTS = 3;
+        globals.parameters.A_PTS = 1;
+        globals.parameters.B_PTS = 1;
+        globals.parameters.DIST = 0.5;
+        globals.parameters.K_PTS = 1;
+        globals.parameters.C_PTS = 1;
+        globals.parameters.D_PTS = 1;
+        globals.parameters.E_PTS = 1;
+        globals.parameters.F_PTS = 1;
+        globals.parameters.G_PTS = 1;
+        globals.parameters.LENGTH2 = 0.2;
+        globals.parameters.RADIUS2 = 2.0;
+        globals.parameters.AREA2 = 1.0;
+    
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 15; j++) {
+                globals.lcm[i][j] = GlobalDeclarations.Connectors.ORR; 
+            }
+        }
+    
+        // Call the DECIDE function
+        Main.DECIDE(globals);
+
+        assertFalse(globals.launchDecision);
+    }
+
+    @Test
+    public void testInInValidIputForDecide2(){
+        GlobalDeclarations.Globals globals = new GlobalDeclarations.Globals();
+    
+        // Set number of points
+        globals.numPoints = 14;
+        globals.xCoordinates = new double[]{0, 1, 2, 3, 4, 0, -1, 2, 3, 0, 0, 0, 12, 12};
+        globals.yCoordinates = new double[]{0, 1, 0, 1, 0, 0, 2, 1, -2, 0, 0, 0, 12, 12};
+
+        // Set LIC parameters to ensure conditions are not met
+        globals.parameters.LENGTH1 = 0.5;   
+        globals.parameters.RADIUS1 = 1.0;   
+        globals.parameters.EPSILON = 0.1;   
+        globals.parameters.AREA1 = 100;     
+        globals.parameters.Q_PTS = 3;
+        globals.parameters.QUADS = 1;
+        globals.parameters.N_PTS = 3;
+        globals.parameters.A_PTS = 1;
+        globals.parameters.B_PTS = 1;
+        globals.parameters.DIST = 0.5;
+        globals.parameters.K_PTS = 1;
+        globals.parameters.C_PTS = 1;
+        globals.parameters.D_PTS = 1;
+        globals.parameters.E_PTS = 1;
+        globals.parameters.F_PTS = 1;
+        globals.parameters.G_PTS = 1;
+        globals.parameters.LENGTH2 = 0.2;
+        globals.parameters.RADIUS2 = 2.0;
+        globals.parameters.AREA2 = 1.0;
+    
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 15; j++) {
+                globals.lcm[i][j] = GlobalDeclarations.Connectors.ANDD;
+            }
+        }
+    
+        // Call the DECIDE function
+        Main.DECIDE(globals);
+
+        assertFalse(globals.launchDecision);
     }
 }
